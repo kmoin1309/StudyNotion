99ac038ade67233945914c8de5ca5ba47476977d
Quazi Muhammad Moinuddiin <128276854+kmoin1309@users.noreply.github.com>
2024-03-12 00:28:21 +0530

    first iteration

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+
+
+function App() {
+
+  return (
+   <div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+   </div>
+  );
+}
+
+export default App;
