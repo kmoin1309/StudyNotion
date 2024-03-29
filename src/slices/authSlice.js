a06ed2ea970133bfba7600ffe6741cccf199ac56
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-03-29 11:12:59 +0530

    fix:backend ,authetication and slices fixed

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  signupData: null,
+  loading: false,
   token: localStorage.getItem("token")
     ? JSON.parse(localStorage.getItem("token"))
     : null,
@@ -10,11 +12,18 @@ const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
+    setSignupData(state, value) {
+      state.signupData = value.payload;
+    },
+    setLoading(state, value) {
+      state.loading = value.payload;
+    },
     setToken(state, value) {
       state.token = value.payload;
     },
   },
 });
 
-export const { setToken } = authSlice.actions;
+export const { setSignupData, setLoading, setToken } = authSlice.actions;
+
 export default authSlice.reducer;
